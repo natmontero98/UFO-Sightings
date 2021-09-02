71c6c33923c3f425fa42910013c1344278d80e10
natmontero98 <85467925+natmontero98@users.noreply.github.com>
2021-09-02 11:10:21 -0700

    complete JavaScript code

--- app.js
+++ app.js
@@ -24,3 +24,30 @@ function buildTable(data) {
       );
     });
   }
+
+function handleClick() {
+    // Grab the datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+  
+     // Check to see if a date was entered and filter the
+    // data using that date.
+    if (date) {
+      // Apply `filter` to the table data to only keep the
+      // rows where the `datetime` value matches the filter value
+      filteredData = filteredData.filter(row => row.datetime === date);
+    };
+  
+     // Rebuild the table using the filtered data
+    // @NOTE: If no date was entered, then filteredData will
+    // just be the original tableData.
+    buildTable(filteredData);
+};
+// we're telling D3 to execute our handleClick() function 
+// when the button with an id of filter-btn is clicked.
+// "click is an event D3 listens to"
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build the original table when the page loads
+buildTable(tableData);
+
